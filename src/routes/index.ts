df1959c8528f283e4c8b8623d7eff0d7c46198d6
RewanNaguib <rewan.mnaguib@gmail.com>
2022-10-25 00:14:21 +0200

    adding all the validations needed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,69 @@
 import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import ImageProcessing from '../utils/imageProcessing';
 
 const routes = express.Router();
 
-routes.get('/image', (req, res)=>{
-    res.send('routesssss');
+routes.get('/image', async(req, res): Promise<void>=>{
+
+    const width =  parseInt(req.query.width  as string);
+    const height = parseInt(req.query.height as string);
+    const filename = (req.query.filename as unknown) as string;
+    
+    //check that the width the user enters > 0
+    if(width <= 0)
+    {
+        res.status(400).send('Width should be greather than 0');
+    } 
+    
+    //check that the width the user enters is only type number
+    if(isNaN(width)) 
+    {
+        res.status(400).send('Width should be type number only');
+    }
+
+    //check that the height the user enters > 0
+    if(height <= 0)
+    {
+        res.status(400).send('Height should be greater than 0');
+    }
+
+    //check that the height the user enters is only type number
+    if(isNaN(height)) 
+    {
+        res.status(400).send('Height should be type number only');
+    }
+
+
+    if(filename) 
+    {
+        const createdImage:string  = path.join(__dirname, '../', '../', 'images/', 'createdImages/', filename) + '_' + `${width}` + '_' + `${height}`  + '.jpg';
+
+        if(fs.existsSync(createdImage))
+        {
+            res.sendFile(createdImage);
+        }
+        else 
+        {
+            const finalImage = await ImageProcessing(width, height, filename);
+
+            if (!String(finalImage).includes('error')) 
+            {
+                res.sendFile(finalImage);
+            }
+
+            else
+            {
+                res.status(404).send('Check the filename you entered');
+            }
+        }
+    }
+    else
+    {
+        res.status(500).send('You should enter at least a filename in the query string');
+    }
+
 });
 
 export default routes;
--- src/utils/imageProcessing.ts
+++ src/utils/imageProcessing.ts
@@ -2,11 +2,6 @@ import sharp from 'sharp';
 import path from 'path';
 import fs from 'fs';
 
-// create a variable that get the original image name from it's path (images/originalImages)
-// create a variable that create a new image name (originalname_width_height.jpg) (images/createdImages)
-// create a folder for the created images if it doesn't exist (images/createdImages)
-// resize the image based on the query parameters sent in the URL (filename, height, width) => using sharp
-
 const ImageProcessing = async(width:number, height:number, filename:string) : Promise<string> =>{
     const originalImage:string = path.join(__dirname, '../', '../', 'images/', 'originalImages/', filename) + '.jpg';
     const createdImage:string  = path.join(__dirname, '../', '../', 'images/', 'createdImages/', filename) + '_' + `${width}` + '_' + `${height}`  + '.jpg';
@@ -14,27 +9,28 @@ const ImageProcessing = async(width:number, height:number, filename:string) : Pr
 
     if(!fs.existsSync(createdImagesFolder))
     {
-        await fs.mkdirSync(createdImagesFolder);
+         fs.mkdirSync(createdImagesFolder);
     }
 
     try {
+        // check if no width and no height sent
             if(!width && !height)
             {
                 return originalImage;
             }
-        
+        // check if only width is sent
             else if(width && !height) 
             {
                 await sharp(originalImage).resize(width).toFile(createdImage);
                 return createdImage;
             }
-        
+        // check if only height is sent
             else if(!width && height)
             {
                 await sharp(originalImage).resize(height).toFile(createdImage);
                 return createdImage;
             }
-        
+        // check if both width and height are sent
             else 
             {
                 await sharp(originalImage).resize(width, height).toFile(createdImage);
@@ -43,8 +39,9 @@ const ImageProcessing = async(width:number, height:number, filename:string) : Pr
             }
     }
     catch(error) {
-        return error;
+        return 'error';
     }
+
 };
 
 export default ImageProcessing;

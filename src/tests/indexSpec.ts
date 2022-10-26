a58bedb39d18cbe1e99a3afb77f02f74d6884e74
RewanNaguib <rewan.mnaguib@gmail.com>
2022-10-26 16:43:48 +0200

    removing done from test cases

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -2,97 +2,86 @@ import supertest from 'supertest';
 import app from '../index';
 import fs from 'fs';
 import path from 'path';
+import ImageProcessing from '../utils/imageProcessing';
 
 const request = supertest(app);
 describe('Test URL Response Status Code', () => {
-  it("It should return Error 500 if the user didn't include filename in the URL query parameters", async (done) => {
+  it("It should return Error 500 if the user didn't include filename in the URL query parameters", async () => {
     const response = await request.get('/api/image');
     expect(response.status).toBe(500);
-    done();
   });
 
-  it("It should return Error 500 if the user didn't include width and height only in the URL query parameters", async (done) => {
+  it("It should return Error 500 if the user didn't include width and height only in the URL query parameters", async () => {
     const response = await request.get('/api/image?width=100&height=100');
     expect(response.status).toBe(500);
-    done();
   });
 
-  it('It should return SUCCESS 200 if the user include filename only in the URL query parameters', async (done) => {
+  it('It should return SUCCESS 200 if the user include filename only in the URL query parameters', async () => {
     const response = await request.get('/api/image?filename=fjord');
     expect(response.status).toBe(200);
-    done();
   });
 
-  it('It should return SUCCESS 200 if the user include filename & width only in the URL query parameters', async (done) => {
+  it('It should return SUCCESS 200 if the user include filename & width only in the URL query parameters', async () => {
     const response = await request.get('/api/image?filename=fjord&width=100');
     expect(response.status).toBe(200);
-    done();
   });
 
-  it('It should return SUCCESS 200 if the user include filename & height only in the URL query parameters', async (done) => {
+  it('It should return SUCCESS 200 if the user include filename & height only in the URL query parameters', async () => {
     const response = await request.get('/api/image?filename=fjord&height=100');
     expect(response.status).toBe(200);
-    done();
   });
 
-  it('It should return SUCCESS 200 if the user include filename & width & height in the URL query parameters', async (done) => {
+  it('It should return SUCCESS 200 if the user include filename & width & height in the URL query parameters', async () => {
     const response = await request.get(
       '/api/image?filename=fjord&width=100&height=100'
     );
     expect(response.status).toBe(200);
-    done();
   });
 });
 
 describe('Test Error Message Validations', () => {
-  it('Gets Error Message if the user enters width value equals to or less than zero', async (done) => {
+  it('Gets Error Message if the user enters width value equals to or less than zero', async () => {
     const response = await request.get(
       '/api/image?filename=fjord&width=0&height=100'
     );
     expect(response.text).toBe('Width should be greather than 0');
-    done();
   });
 
-  it('Gets Error Message if the user enters height value equals to or less than zero', async (done) => {
+  it('Gets Error Message if the user enters height value equals to or less than zero', async () => {
     const response = await request.get(
       '/api/image?filename=fjord&width=100&height=0'
     );
     expect(response.text).toBe('Height should be greater than 0');
-    done();
   });
 
-  it('Gets Error Message if the user enters any thing rather than a number for the width value', async (done) => {
+  it('Gets Error Message if the user enters any thing rather than a number for the width value', async () => {
     const response = await request.get(
       '/api/image?filename=fjord&width=xyz&height=100'
     );
     expect(response.text).toBe('Width should be type number only');
-    done();
   });
 
-  it('Gets Error Message if the user enters any thing rather than a number for the height value', async (done) => {
+  it('Gets Error Message if the user enters any thing rather than a number for the height value', async () => {
     const response = await request.get(
       '/api/image?filename=fjord&width=100&height=xyz'
     );
     expect(response.text).toBe('Height should be type number only');
-    done();
   });
 
-  it('Gets Error Message if the user enters a not valid filename', async (done) => {
+  it('Gets Error Message if the user enters a not valid filename', async () => {
     const response = await request.get(
       '/api/image?filename=xyz&width=100&height=100'
     );
     expect(response.text).toBe('Check the filename you entered');
-    done();
   });
 
-  it("Gets Error Message if the user didn't enter a filename", async (done) => {
+  it("Gets Error Message if the user didn't enter a filename", async () => {
     const response = await request.get(
       '/api/image?filename=&width=100&height=100'
     );
     expect(response.text).toBe(
       'You should enter at least a filename in the query string'
     );
-    done();
   });
 });
 
@@ -104,11 +93,10 @@ describe('Test Image Processing Used By Sharp', ()=>{
         'images/',
         'createdImages/'
       );
-    it('Gets The new name of the created image in createdImages directory', async(done)=>{
+    it('Gets The new name of the created image in createdImages directory', async()=>{
         const response = await request.get(
             '/api/image?filename=fjord&width=105&height=105'
         );
         expect(fs.existsSync(createdImagesFolder + '/fjord_105_105.jpg')).toBe(true);
-        done();
     });
 });
